ab667c4d6a91cd8bbd41d4baed40eb1b6c12aebb
Claudio Moura <claudio.moura@sapo.pt>
2016-02-15 19:26:54 +0000

    Update main_with_%.py

--- extensions/virustotal/main_with_%.py
+++ extensions/virustotal/main_with_%.py
@@ -1,25 +1,23 @@
-import xml.etree.ElementTree as ET
+# -*- coding: utf-8 -*-
 import time
-import simplejson as json
+# import simplejson as json
+import json
 import urllib
 import urllib2
+import os
 from datetime import datetime
+from vito_config import *
 
 
 # Scan folder searching queue files from syscheck in syscheck folder
-def get_queue_files():
- 	
- 	with open('local_data.json', 'r') as data_file: 
- 		tree_data = json.load(data_file)
- 		dir = tree_data["local_conf_vt"]["syscheck_folder"]
-
-	import os
+def get_queue_files(): 
+	
 	list_files = []
 
 	# Check and filter results, all queue files are called: "(agent_name) 192.0.0.0-_syscheck"
-	for file in os.listdir(dir):
+	for file in os.listdir(syscheck_folder):
 		if file[-10:] == "-_syscheck":
-			file = dir+file
+			file = syscheck_folder+file
 			list_files.append(file)
 
 	return list_files
@@ -30,8 +28,11 @@ def scan_queue_file(file_path, last_entry, countZ):
 
 	file = open(file_path, 'r').readlines()
 
-	#get (vt API key, vt restrintions)
-	vt_config = extract_vt_config()
+	# #get (vt API key, vt restrintions)
+	# vt_config = extract_vt_config()
+	
+	if is_public_key:
+		frecuency_key = 15
 
 	count = 0
 	update = False
@@ -39,29 +40,29 @@ def scan_queue_file(file_path, last_entry, countZ):
 		line = line.split(':',5)		
 		md5sum = line[4]
 		date_and_name = line[5].split('!')[1]
-		print "info this line:", md5sum, date_and_name
+		# print "info this line:", md5sum, date_and_name
 		# example output: "1427747294 /home/user/myFiles/3.file"
 
 		#checked already, leaving this agent syscheck file
 		if last_entry == date_and_name:
-			print "EXIT FOR THIS FILE----------------------@\n"
+			# print "EXIT FOR THIS FILE----------------------@\n"
 			break
 
 		if (countZ == 0 and count ==0) is False :
-			time.sleep(vt_config[1])
+			time.sleep(frecuency_key)
 
 		if count == 0:
 			new_last_entry = date_and_name
 			update = True
 		count += 1
 		
-		results_vt = retrieve_results_md5(md5sum, vt_config[0])
+		results_vt = retrieve_results_md5(md5sum, personal_API_Key)
 
 		resp_code = results_vt.get("response_code")
 
 		if resp_code == -1:
 			#Something went wrong ... aborting scan
-			puke_error_log()
+			puke_error_log(696)
 			update = False
 			break
 			
@@ -79,49 +80,10 @@ def scan_queue_file(file_path, last_entry, countZ):
 				#data in vt
 				puke_log(hostname, file_name, file_time, results_vt)
 
-
-		# #send md5 to vt and parser results
-		# # print retrieve_results_md5(md5sum, myPublicKey)
-		# data_vt = parser_json_result( retrieve_results_md5(md5sum, vt_config[0]) )
-		
-		# print data_vt
-		# #if there is any positive result
-		# if data_vt is False:
-		# 	print "Non data in VT or Connection Error"
-		# 	"""
-		# 	"error connection with VirusTotal DB, please check your internet connection or VirusTotal API Key"
-		# 	***Abort this file
-		# 	***not update local_data.json
-		# 	"""
-		# elif data_vt[0] == 0:
-		# 	print "Good File"
-		# elif data_vt[0] > 0:
-		# 	print "------->I AM A VIRUS<--------"
-
-		# 	#Create LOG : extract name agent, ip, file name and date
-		# 	new_log = file_path.split('/')[-1], data_vt[0], data_vt[1], date_and_name
-		# 	write_log(new_log)
-		# 	"""
-		# 	hostname = file_path.split('/')[-1]
-		# 	puke_log(hostname)
-		# 	"""
 	
 	#if true: update_db(file_path, new_last_entry)
 	if update is True:
 		update_db(file_path, new_last_entry)
-
-# ###OBSOLOTE### Write the log in our file "./log_vt" 
-def write_log(info_list):
-	
-	timestamp = datetime.fromtimestamp(  int(info_list[3].split(' ')[0])  ).strftime('%Y-%m-%d %H:%M:%S')
-	file = info_list[3].split(' ')[1].strip()
-	timenow = datetime.now().strftime('%Y.%m.%d %H:%M:%S')
-
-	#[Time ][Host ] [Sender virustotal-devel][Log: Suspicious file identified. Ratio detected: 4/42|  /file.txt |Created date: 2013...  ]
-	log_string = "[Timestamp " + timenow +"] [Sender virustotal-devel] [Host " + info_list[0][:-10] + "] [Log: Suspicious file identified. Ratio detected: " + str(info_list[1]) + '/' + str(info_list[2]) +"|" + file + "|Created date: " + timestamp + "]\n" 
-	# print log_string
-	with open('log_vt', 'ab') as data_file:
-		data_file.write(log_string)
 
 #create a log from vt results
 def puke_log(hostname, file_name, file_time, log_json):
@@ -170,25 +132,33 @@ def puke_log(hostname, file_name, file_time, log_json):
 
 	write_anylog( "[Time "+ time_now() + "] [Sender virustotal-devel]  [Message " + message + "]  [Host " + hostname + "] [Percent " + percent_msg + "]\n"  )
 
-# Translate the percent of dangerousness to human text message
+
+# Translate the percent of dangerousness to text message
 # Switch case % for level of dangerousness
 def get_percent(prcnt):
   while True:
     if prcnt>=0 and prcnt<11:
-        print ("0­10% Malicious file")
+        print ("0­ - 10% Malicious file")
     elif prcnt>10 and prcnt<31:
-        print ("11­30% Dangeruos file")
+        print ("11­ - 30% Dangeruos file")
     elif prcnt>30 and prcnt<61:
-        print ("31­60% Your system is in danger")
+        print ("31­ - 60% Your system is in danger")
     elif prcnt>60 and prcnt<101:
-        print ("61­100% you are fucked")
+        print ("61­ - 100% you are fucked")
     else:
         print ("Error with percent!!")
     break
+
+
 #create a error log, not internet connection or wrong VT API key
 # [Time 2016.02.03 20:44:04] [Sender virustotal-devel]  [Message ERROR 696: Not connection with VirusTotal DB, please check your internet connection or VirusTotal API Key]
-def puke_error_log():
-	message = "ERROR 696: Not connection with VirusTotal DB, please check your internet connection or VirusTotal API Key"
+def puke_error_log(code):
+
+	if code == 696:
+		message = "ERROR 696: Not connection with VirusTotal DB, please check your internet connection or VirusTotal API Key"
+	elif code == 697:
+		message = "ERROR 697: Wrong API Key, please check your VirusTotal API Key"
+
 	write_anylog( "[Time "+ time_now() + "] [Sender virustotal-devel]  [Message " + message + "]\n" )
 
 #create a no-data in VT log
@@ -202,7 +172,8 @@ def puke_noD_log(hostname, file_name, file_time):
 
 #write a log in our log file "./log_vt
 def write_anylog(log_string):
-	with open('log_vt', 'ab') as data_file:
+	
+	with open(log_file, 'ab') as data_file:
 		data_file.write(log_string)
 
 #conver from epoch time to a human readable time
@@ -236,31 +207,36 @@ def retrieve_results_md5(MD5, myPersonalKey):
 		# return False
 		return {u'response_code': -1, u'more_info': u'error connection with VirusTotal DB, please check your internet connection or VirusTotal API Key'}
 
-# ###OBSOLOTE### Extract info from VT report : positive, total
-def parser_json_result(vt_answer):
-	#if there is any result, response code from vt is 1, otherwise there is not results
-	if vt_answer is False:
-		return False
-	elif vt_answer.get("response_code") == 1:
-		return (vt_answer.get("positives"), vt_answer.get("total"))
-	else:
-		return False
-
 #extract previous last entry for this agent in our json db
 # if there is not any data for this agent, is going to create in our db
 def extract_last_entry(agent_path):
 	agent = agent_path.split('/')[-1]
-	with open('local_data.json', 'r') as data_file:  
-		updated = False  
-		tree_data = json.load(data_file)
-		last_entry_found = tree_data["agents"].get( agent )
-		if last_entry_found == None:
-			tree_data["agents"][ agent ] = "None"
-			updated = True
 
-	if updated is True:
-		with open('local_data.json', 'w') as f:
-			f.write(json.dumps(tree_data, indent=4))
+	with open(db_file, 'r') as data_file:  
+
+		try:
+			tree_data = json.load(data_file)
+		except:
+			#DB is broken: Creating new emptly DB
+			tree_data = {u"agents": {} }
+
+		agent_record = tree_data["agents"].get( agent )
+
+		#If no data in DB for this agent: Creating from scratch
+		if agent_record == None:
+			tree_data["agents"][ agent ] = {}
+			tree_data["agents"][ agent ]["last_entry"] = "None"
+			tree_data["agents"][ agent ]["FTS"] = time_now()
+			last_entry_found = None
+		
+		else:
+			last_entry_found = agent_record.get( "last_entry" )
+
+		tree_data["agents"][ agent ]["LTS"] = time_now()
+
+
+	with open(db_file, 'w') as f:
+		f.write(json.dumps(tree_data, indent=4))
 
 	return last_entry_found
 
@@ -279,14 +255,30 @@ def extract_vt_config():
 def update_db(agent_path, new_last_entry):
 	agent = agent_path.split('/')[-1]
 
-	with open('local_data.json', 'r') as f:
+	with open(db_file, 'r') as f:
 		tree_data = json.load(f)
 
-	with open('local_data.json', 'w+') as f:
-		tree_data["agents"][ agent ] = new_last_entry
+	with open(db_file, 'w+') as f:
+		tree_data["agents"][ agent ]["last_entry"] = new_last_entry
 		f.write(json.dumps(tree_data, indent=4))
 		f.close()
 
+# This function create a service/Daemon that will execute a det. task
+def summon_daemon():
+
+  	try:
+		# Store the Fork PID
+		pid = os.fork()
+
+		
+		if pid > 0:
+			print 'PID: %d' % pid
+			os._exit(0)
+
+	except OSError, error:
+		print 'Unable to fork. Error: %d (%s)' % (error.errno, error.strerror)
+		os._exit(1)
+	main_vt()
 
 ############################################################################
 ############################################################################
@@ -300,10 +292,22 @@ def update_db(agent_path, new_last_entry):
 ############################################################################
 ############################################################################
 
-countZ = 0
-for queue_file in get_queue_files():
-	print "\nSCANING FILE: ", queue_file
-	print "\nLAST ENTRY FOUND: ", extract_last_entry(queue_file)
-	scan_queue_file(queue_file, extract_last_entry(queue_file), countZ)
-	print "-----------END SCAN FILE-------------\n\n\n\n"
-	countZ += 1
+def main_vt():
+	while True:
+		# print "\nRound starts here:\n"
+		countZ = 0
+
+		if len(personal_API_Key) == 64:
+			for queue_file in get_queue_files():
+				# print "\nSCANING FILE: ", queue_file
+				# print "\nLAST ENTRY FOUND: ", extract_last_entry(queue_file)
+				scan_queue_file(queue_file, extract_last_entry(queue_file), countZ)
+				# print "-----------END SCAN FILE-------------\n\n\n\n"
+				countZ += 1
+		else:
+			puke_error_log(697)
+
+		time.sleep(sleep_time)
+
+if __name__ == "__main__":
+	summon_daemon()
